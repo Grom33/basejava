7db7b523ca36294842cfa2b80d796d0ec544b4dd
grom33@mail.ru <grom33@mail.ru>
2018-01-08 17:22:08 +0300

    08/01/2018

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,64 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int countOfResume = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        countOfResume = 0;
     }
 
     void save(Resume r) {
+        if (countOfResume < storage.length + 1) {
+            storage[countOfResume++] = r;
+        } else {
+            System.out.println("Список резюме переполнен!");
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int res = findIndex(uuid);
+        if (res > -1) {
+            return storage[res];
+        } else {
+            return null;
+        }
     }
 
     void delete(String uuid) {
+        if (countOfResume > 0) {
+            int res = findIndex(uuid);
+            if (res < countOfResume - 1) {
+                System.arraycopy(storage, (res + 1), storage, res, (countOfResume - res - 1));
+            }
+            countOfResume--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] res = new Resume[countOfResume];
+        System.arraycopy(storage, 0, res, 0, countOfResume);
+        return res;
     }
 
     int size() {
-        return 0;
+        return countOfResume;
     }
+
+    private int findIndex(String f) {
+        for (int i = 0; i < countOfResume; i++) {
+            if (storage[i].uuid.equals(f)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
