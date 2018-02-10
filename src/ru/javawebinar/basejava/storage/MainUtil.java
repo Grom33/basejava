7bf4182bf4b0eada13b97c8cfd02ab420ffaa501
grom33@mail.ru <grom33@mail.ru>
2018-02-10 16:09:04 +0300

    HW12 10/02/2018 copy / past

--- src/ru/javawebinar/basejava/storage/MainUtil.java
+++ src/ru/javawebinar/basejava/storage/MainUtil.java
@@ -2,10 +2,14 @@ package ru.javawebinar.basejava.storage;
 
 public class MainUtil {
     public static void main(String[] args) {
-        System.out.println(Integer.valueOf(-1) == Integer.valueOf(-1));
-        System.out.println(Integer.valueOf(-1) == new Integer(-1));
-        int result = getInt();
-        System.out.println(result);
+       // System.out.println(Integer.valueOf(-1) == Integer.valueOf(-1));
+       // System.out.println(Integer.valueOf(-1) == new Integer(-1));
+       // int result = getInt();
+       // System.out.println(result);
+        System.out.println(Runtime.getRuntime().freeMemory());
+        System.out.println(Runtime.getRuntime().maxMemory());
+        System.out.println(Runtime.getRuntime().totalMemory());
+
     }
 
     private static Integer getInt() {
